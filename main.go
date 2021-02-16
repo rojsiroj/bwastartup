0bd8bc8c1e6dbb226848e6120057c9771a20221a
rojsiroj <sirojudin.dev@gmail.com>
2021-02-16 22:24:47 +0700

    Login endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bwastartup/handler"
 	"bwastartup/user"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -20,11 +21,23 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+	input := user.LoginInput{
+		Email:    "test@test.com",
+		Password: "test",
+	}
+	user, err := userService.Login(input)
+	if err != nil {
+		fmt.Println("Terjadi kesalahan")
+		fmt.Println(err.Error())
+	}
+	fmt.Println(user.Name)
+	fmt.Println(user.Email)
 	userHandler := handler.NewUserHandler(userService)
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 
 	router.Run()
 }
